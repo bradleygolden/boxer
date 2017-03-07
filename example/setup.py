1e6b8e5d3e70d6e8263e1154679cd024bcda216e
Bradley Golden <golden.bradley@gmail.com>
2017-03-06 23:20:34 -0600

    Initial setup

--- example/setup.py
+++ example/setup.py
@@ -4,9 +4,9 @@ from distutils.core import setup
 
 setup(name='Example App',
       version='1.0',
-      description='An example app illustrating tox with docker',
+      description='An example app illustrating how to use boxer',
       author='Bradley Golden',
       author_email='golden.bradley@gmail.com',
       packages=['app'],
-      url='bradleygolden.com'
+      url='bradleygolden.me'
      )
