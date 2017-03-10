89bc6640b46a810ce7d09ba7aeef9e2bf11ee81f
Bradley Golden <golden.bradley@gmail.com>
2017-03-09 19:22:02 -0600

    update setup config

--- setup.py
+++ setup.py
@@ -9,7 +9,6 @@ setup(
     packages=['boxer'],
     include_package_data=True,
     url='https://github.com/bradleygolden/boxer',
-    download_url='https://github.com/bgolden/boxer/archive/0.1.tar.gz.',
     keywords=['tox', 'pyenv', 'docker'],
     install_requires=[
         'Click',
